c7304eb29ef9759dab2fc0fc255c1b0b0e1a3bed
Tej1106083 <rajput.tejendrasingh@gmail.com>
2014-01-28 05:34:10 -0800

    Create speed_pass.cpp
    Speed Pass provides 2-pass security. It will protect your data even if your password is known to someone. It firstly matches the string entered with the original password.
    If it get a pass through then it will check for the typing speed of the user. If this speed matches the typing speed of the authorized user, it will provide him the instant access.

---
+++ speed_pass.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<dos.h>
+#include<string.h>
+int main()
+{
+	time_t first,last;
+	char ch,password[100];
+	int i;
+	system("cls");
+	printf("\n\n\n\tEnter password to access the database ::\n\t");
+	for(i=0;i<100;i++)
+	{       ch=getch();
+		if(i==0)
+		time(&first);
+		if(ch=='\r')
+		{
+			time(&last);
+			password[i]='\0';
+			if(!strcmp(&password[0],"Tejendra Singh"))
+			{
+				if(difftime(last,first)>1.5&&difftime(last,first)<3.50)
+				{
+				for(i=0;i<11;i++)
+				{
+					system("cls");
+                    time(&first);
+					printf("\n\n\t\t\tWelcome Tejendra Singh");
+					printf("\n\n\t\t\t");
+					puts(ctime(&first));
+					printf("\n\n\t\t\tPlease wait for \n\n\t\t\t%d\n\n\t\t\tSeconds",10-i);
+					if(i==10)
+					printf("\n\n\t\t\tWelcome in the game\n\t\t\tPress any key to continue ");
+					delay(1000);
+				}
+				system("D:\\Game\\Tekken-3");
+				}
+				else
+				{printf("\n\n\t\tWelldone, But I think you are not supposed to enter the database"); getch(); main(); break;}
+			}
+			else
+			{printf("\n\n\t\tSorry, Wrong Password"); getch(); main(); break;}
+			break;
+		}
+		else
+		{password[i]=ch; printf("%c",ch);}
+		}
+}
